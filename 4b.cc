8beaaa04e2f19344d91c22e3629ef3e5bc3e2c78
Kyle <kpseipp@gmail.com>
2015-05-15 06:02:59 -0700

    now it actually works

--- 4b.cc
+++ 4b.cc
@@ -32,7 +32,7 @@ float mod_by_one(float x)
 
 float f(float y)
 {
-    return std::cos(2*3.141592653589793*y)/depth;
+    return std::cos(2*3.141592653589793*y);
 }
 /*
 height is how far into the tree we are, starting at one
@@ -42,7 +42,9 @@ sum_above is the sum of the f function on above values
 void recurse(int height, float node, float sum_above)
 {   
     float temp;
-    temp = f(node) + sum_above;
+    // printf("recurse call: height: %d node: %f sum_above: %f\n",height,node,sum_above);
+    // printf("best so far %f\n", best_so_far);
+    temp = (f(node) + (height-1)*sum_above)/(height);
     // printf("temp = %f\n", temp);
     if (height == depth)
     {
@@ -53,13 +55,17 @@ void recurse(int height, float node, float sum_above)
     }
     else
     {
-        if (temp + depth - height < best_so_far)
+        // printf("best so far %f\n", best_so_far);
+        // printf("heuristic %f\n",temp + depth - height);
+        if ((temp*height + (depth - height))/depth <best_so_far)
         {
             /* skip this subtree */
+            // printf("skip\n");
         }
         else
         {
             /* investigate this subtree */
+            // printf("recurse\n");
             recurse(height + 1, mod_by_one(node * 5.0/2.0), temp); //leftchild
             recurse(height + 1, mod_by_one((node * 5.0/2.0) +1.0/2.0), temp); //rightchild
         }
@@ -70,12 +76,13 @@ void recurse(int height, float node, float sum_above)
 int main(int argc, char const *argv[])
 {
     depth = atoi(argv[1]);
-    while(depth < 60)
+    while(depth < 71)
     {
         best_so_far = -1*depth;
         recurse(1,1.0/2.0,0);
         printf("At L = %d. F(L) = %f\n", depth, best_so_far);
         depth ++;
+        // break;
     }
     // l is fixed for each value of F(l) that we compute
     return 0;
